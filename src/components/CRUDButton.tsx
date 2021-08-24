4f167238d32a4a4d76002fd5f8134981f43806f8
Abner Silva <abner.js05@gmail.com>
2021-08-24 16:09:50 -0300

    #46623# botao de novo com status de ativo personalizado

--- src/components/CRUDButton.tsx
+++ src/components/CRUDButton.tsx
@@ -1,10 +1,12 @@
 import { Icon } from '@iconify/react';
-import React from 'react'
+import React, { useState } from 'react'
 import './CRUDButton.css'
 
 const CRUDButton = (props: {content?:string}) => {
+    const [active, setActive] = useState(false)
+
     return (
-        <button className="CRUDButton">
+        <button className={`CRUDButton${active?' active':''}`} onClick={() => setActive(true)} >
             <Icon icon="fluent:add-16-regular" width={25} className="icon" />
             {props.content}
         </button>
