64932e5bd76aadef9e599efa352ae3d36ec997fc
abnerjs <abner@datamob.com.br>
2021-10-07 13:47:41 -0300

    #46622# header ultimo acesso text-align

--- src/components/Subtitle.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Subtitle.css';
-
-const Subtitle = (props: { content: string }) => {
-  return <div className="Subtitle">{props.content}</div>;
-};
-
-export default Subtitle;
