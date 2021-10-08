0bc8b6121768d472d668fc66ede328cda55204e5
Matheus Camelucci <matheus@datamob.com.br>
2021-10-08 12:13:27 -0300

    Chamado #46766: Configurada a biblioteca Redux-Saga

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,22 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+
 import { configureStore } from '@reduxjs/toolkit';
 
 import rootReducer from 'src/store/ducks/rootReducer';
+import rootSaga from 'src/store/ducks/rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => {
+    return [...getDefaultMiddleware({ thunk: false }), sagaMiddleware];
+  },
   devTools: true,
 });
+
+sagaMiddleware.run(rootSaga);
 
 export type AppState = ReturnType<typeof store.getState>;
 
