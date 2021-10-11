d42812f770dcb99465052e722ad62a5701235be4
Matheus Camelucci <matheus@datamob.com.br>
2021-10-11 17:15:22 -0300

    Chamado #46766: Ajustes no cabeçalho e nos menus de usuário e páginas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,16 +3,27 @@ import './Header.css';
 import ProfileMenu from './ProfileMenu';
 import { Typography } from '@mui/material';
 
+import { useAppDispatch } from 'src/store';
+import { logout } from 'src/store/ducks/login';
+
 const Header = (props: { title: any }) => {
+  const dispatch = useAppDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <div className="Header">
       <Typography variant="h5">{props.title}</Typography>
       <div className="right">
         <div className="acesso">
           <Typography variant="subtitle1">Último acesso</Typography>
-          <Typography variant="subtitle1">30/06/2021 17:36</Typography>
+          <Typography variant="subtitle1">
+            {new Date().toLocaleString()}
+          </Typography>
         </div>
-        <ProfileMenu />
+        <ProfileMenu onLogout={handleLogout} />
       </div>
     </div>
   );
