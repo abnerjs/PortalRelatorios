5389a9e4881199d3f65c4e6dd8426cd0f88660d6
Abner Silva <abner@datamob.com.br>
2021-10-11 14:21:12 -0300

    submenu com material ui

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,9 +3,43 @@ import { Link } from 'react-router-dom';
 import DatamobIcon from '../assets/DatamobIcon';
 import './Menu.css';
 import { Icon } from '@iconify/react';
+import { Box, Drawer, List, ListItem, ListItemText } from '@mui/material';
 
 const Menu = () => {
   const [active, setActive] = useState(window.location.pathname);
+  const [state, setState] = React.useState(false);
+
+  const toggleDrawer =
+    (open: boolean) =>
+      (event: React.KeyboardEvent | React.MouseEvent) => {
+        if (
+          event.type === 'keydown' &&
+          ((event as React.KeyboardEvent).key === 'Tab' ||
+            (event as React.KeyboardEvent).key === 'Shift')
+        )
+          return;
+
+        setState(open);
+      };
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {['Usuários', 'Empresas', 'Perfis'].map((text, index) => (
+          <Link to={'/' + text.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")}>
+            <ListItem button key={text} onClick={() => setActive('/' + text.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, ""))} >
+              <ListItemText primary={text} className={active === '/' + text.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "") ? 'active' : ''} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
     <div className="Menu">
@@ -24,41 +58,42 @@ const Menu = () => {
             <Icon icon="fluent:home-16-regular" />
           </div>
         </Link>
-        <Link to="/usuarios">
+        <Link to="/documentos">
           <div
             className={
               `menuButton` + (active === '/documentos' ? ' active' : '')
             }
-            onClick={() => setActive('/usuarios')}
+            onClick={() => setActive('/documentos')}
           >
             <Icon icon="fluent:document-bullet-list-20-regular" />
           </div>
         </Link>
-        <Link to="/usuarios">
-          <div
-            className={`menuButton` + (active === '/usuarios' ? ' active' : '')}
-            onClick={() => setActive('/usuarios')}
-          >
-            <Icon icon="fluent:person-20-regular" />
-          </div>
-        </Link>
-        <Link to="/perfis">
-          <div
-            className={`menuButton` + (active === '/perfis' ? ' active' : '')}
-            onClick={() => setActive('/perfis')}
-          >
-            <Icon icon="fluent:briefcase-20-regular" />
-          </div>
-        </Link>
+        <div
+          className={`menuButton` + (active === '/usuarios' || active === '/perfis' || active === '/empresas' ? ' active' : '')}
+          onClick={() => setState(true)}
+        >
+          <Icon icon="fluent:person-20-regular" />
+        </div>
         <Link to="/usuarios">
           <div
             className={`menuButton` + (active === '/logs' ? ' active' : '')}
-            onClick={() => setActive('/usuarios')}
+            onClick={() => {
+              setActive('/usuarios');
+            }}
           >
             <Icon icon="fluent:notebook-24-regular" />
           </div>
         </Link>
       </div>
+
+      <Drawer
+        anchor="left"
+        open={state}
+        onClose={toggleDrawer(false)}
+      >
+        {list()}
+      </Drawer>
+
     </div>
   );
 };
