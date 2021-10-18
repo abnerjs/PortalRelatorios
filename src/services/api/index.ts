18e33f3b95bfeaf23df65d963d7baf17bedbcdac
Matheus Camelucci <matheus@datamob.com.br>
2021-10-18 13:30:06 -0300

    Chamado #46622: Adicionadas as configurações de autenticação e paginação para a comunicação com a API

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,45 @@
 import axios from 'axios';
 
+import store from 'src/store';
+import { logout } from 'src/store/ducks/login';
+
 const api = axios.create({
   baseURL: 'http://datamob.servehttp.com:8080/Ester/Relatorios/ApiRest/api',
 });
+
+api.interceptors.request.use(
+  (request) => {
+    const state = store.getState();
+    const session = state.session;
+
+    if (session.authenticated) {
+      request.headers = {
+        ...request.headers,
+        Authorization: `Bearer ${session.user?.accessToken}`,
+      };
+    }
+
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    if (response.status === 401) {
+      store.dispatch(logout());
+    }
+
+    if (response.status === 403) {
+    }
+
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
